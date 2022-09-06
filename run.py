afa8630becb5eaa8fc4b809c9fd1f6a88e1fe41b
Bram Wasti <bwasti@fb.com>
2022-09-06 06:31:18 -0700

    optional show cmd

--- run.py
+++ run.py
@@ -8,11 +8,16 @@ cmds = [
   ["pytorch (python): ", "python test_pt.py",u"\u001b[35;1m"],
   ["shumai (js):      ", "bun test_sm.js", u"\u001b[36;1m"],
 ]
-if len(sys.argv) == 2:
+show_cmd = False
+if len(sys.argv) >= 2:
   if sys.argv[1] == "pytorch":
     cmds = [cmds[1], cmds[2]]
   elif sys.argv[1] == "tensorflow":
     cmds = [cmds[0], cmds[2]]
+  elif sys.argv[1] == "show_cmd":
+    show_cmd = True
+elif len(sys.argv) == 3 and sys.argv[2] == "show_cmd":
+  show_cmd = True
 
 def run(desc, op, sizes):
   print(f"\033[4m\033[1m{desc}\033[0m\n")
@@ -34,7 +39,7 @@ def run(desc, op, sizes):
         pass
       t1 = time.time()
       print(f"{out if out else 'error'} ({t1-t0:.2f} seconds total, including init and warmup)", end='')
-      print("\033[2m\t", full_cmd, u"\u001b[0m")
+      print("\033[2m\t", full_cmd if show_cmd else "", u"\u001b[0m")
     if len(vals) == 2:
       print(f"   \033[1mdifference:        {vals[1]/vals[0]:.2f}x\033[0m")
     print()
